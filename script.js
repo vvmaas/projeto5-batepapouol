c6f796360febce537ea7f80c8d9c1d01eff34e5f
vvmaas <vhvermaas@gmail.com>
2022-06-22 12:42:53 -0300

    logIn menu and sidebar added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const username = prompt("Qual seu nome ?")
+const sideBar = document.querySelector(".sideBar")     
+const sideBar_backg = document.querySelector(".sideBar_background")
+
+
+
+//const username = prompt("Qual seu nome ?") // use input value of logIn screen instead
+
+function logIn() {
+    const loginScreen = document.querySelector(".logIn_screen")
+    loginScreen.classList.add("hidden")
+}
+
+function sideBarIn() {
+    sideBar.classList.add("toScreen")
+    sideBar_backg.classList.remove("hidden")
+}
+
+function sideBarOut() {
+    sideBar.classList.remove("toScreen")
+    sideBar_backg.classList.add("hidden")
+}
